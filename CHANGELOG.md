962be52603a6ec6d81ac60b025281f6ff9703814
sakurawald <sakurawald@gmail.com>
2024-10-29 12:05:15 +0800

    fix: use existing functions to compute the translatable key for block entities.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,5 @@
 > The version number of fuji follows `semver` now: https://semver.org/ 
  
-cherry-pick commits from `fuji v4.2.1`:
-
-- docs: simplify the docs
-  - use a fancy header to reflect the structure of the document. 
-  - add the `listing of commands`.
-  - remove the usage of levels that deepen than h6.
-  - box the code fence.
-- i18n: translation fix for `id_ID` and `zh_TW`. (contributor: @yichifauzi)
-- fix: ensure the commands are executes in main thread. (command_meta.delay module)
-- fix: the lore of meta-data doesn't show in `/fuji inspect fuji-commands` gui. (fuji module)
 
 - fix: possible to occur `Not a JSON Object: null` when a new fake-player is spawned via `carpet` mod. (placeholder module)
+- fix: can't display a specific type of block entity properly, e.g. beds, banners etc. (chunks module)
--- src/main/java/io/github/sakurawald/core/structure/TypeFormatter.java
+++ src/main/java/io/github/sakurawald/core/structure/TypeFormatter.java
@@ -7,22 +7,10 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @UtilityClass
 public class TypeFormatter {
-
-    public static final HashMap<String, String> type2transform_type = new HashMap<>() {
-        {
-            this.put("block.minecraft.mob_spawner", "block.minecraft.spawner");
-            this.put("block.minecraft.brushable_block", "block.minecraft.suspicious_sand");
-            this.put("block.minecraft.sign", "block.minecraft.oak_sign");
-            this.put("block.minecraft.bed", "block.minecraft.white_bed");
-            this.put("block.minecraft.skull", "block.minecraft.player_head");
-            this.put("block.minecraft.banner", "block.minecraft.banner.base.white");
-        }
-    };
 
     public static @NotNull Text formatTypes(ServerCommandSource source, Map<String, Integer> type2amount) {
         MutableText ret = Text.empty();
