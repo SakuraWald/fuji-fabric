beb0713a5d5abaf2d186eccc56a5c45747f2c6b8
FishyFinn <hifinn1234@gmail.com>
2024-12-24 09:32:45 +0800

    added LP Meta for custom command_warmup times

--- src/main/java/io/github/sakurawald/module/mixin/teleport_warmup/ServerPlayerMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/teleport_warmup/ServerPlayerMixin.java
@@ -1,27 +1,31 @@
 package io.github.sakurawald.module.mixin.teleport_warmup;
 
 import io.github.sakurawald.core.auxiliary.minecraft.EntityHelper;
+import io.github.sakurawald.core.auxiliary.minecraft.PermissionHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.RegistryHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.TextHelper;
 import io.github.sakurawald.core.manager.Managers;
 import io.github.sakurawald.core.structure.SpatialPose;
 import io.github.sakurawald.core.structure.TeleportTicket;
 import io.github.sakurawald.module.initializer.teleport_warmup.TeleportWarmupInitializer;
+import net.minecraft.network.packet.s2c.play.PositionFlag;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Mixin(value = ServerPlayerEntity.class, priority = 1000 - 500)
 public abstract class ServerPlayerMixin {
 
-    @Inject(method = "teleport(Lnet/minecraft/server/world/ServerWorld;DDDFF)V", at = @At("HEAD"), cancellable = true)
-    public void interceptTeleportAndAddTicket(@NotNull ServerWorld targetWorld, double x, double y, double z, float yaw, float pitch, @NotNull CallbackInfo ci) {
+    @Inject(method = "teleport", at = @At("HEAD"), cancellable = true)
+    public void interceptTeleportAndAddTicket(ServerWorld serverWorld, double x, double y, double z, Set<PositionFlag> set, float yaw, float pitch, boolean bl, CallbackInfoReturnable<Boolean> cir) {
         // check blacklist
-        if (!TeleportWarmupInitializer.config.model().dimension.blacklist.contains(RegistryHelper.ofString(targetWorld))) {
+        if (!TeleportWarmupInitializer.config.model().dimension.blacklist.contains(RegistryHelper.ofString(serverWorld))) {
             return;
         }
 
@@ -30,21 +34,25 @@ public abstract class ServerPlayerMixin {
         // If we try to spawn a fake-player in the end or nether, the fake-player will initially spawn in overworld
         // and teleport to the target world. This will cause the teleport warmup to be triggered.
         if (!EntityHelper.isRealPlayer(player)) return;
-
         TeleportTicket ticket = TeleportWarmupInitializer.getTeleportTicket(player);
+
+        Optional<Integer> permission_warmup_time = PermissionHelper.getMeta(player.getUuid(), "fuji.teleport_warmup.warmup", Integer::valueOf);
+        //set warmup seconds to LP permission seconds or default config seconds
+        int warmup_seconds = permission_warmup_time.orElse(TeleportWarmupInitializer.config.model().warmup_second);
+
         if (ticket == null) {
             ticket = TeleportTicket.make(
                 player
                 , SpatialPose.of(player)
-                , new SpatialPose(targetWorld, x, y, z, yaw, pitch)
-                , TeleportWarmupInitializer.config.model().warmup_second * 1000
+                , new SpatialPose(serverWorld, x, y, z, yaw, pitch)
+                , warmup_seconds * 1000
                 , TeleportWarmupInitializer.config.model().interruptible
             );
             Managers.getBossBarManager().addTicket(ticket);
-            ci.cancel();
+            cir.cancel();
         } else if (!ticket.isCompleted()) {
             TextHelper.sendActionBarByKey(player, "teleport_warmup.another_teleportation_in_progress");
-            ci.cancel();
+            cir.cancel();
         }
 
         // yeah, let's do teleport now.
