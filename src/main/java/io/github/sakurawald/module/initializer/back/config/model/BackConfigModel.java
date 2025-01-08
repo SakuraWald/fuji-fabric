58c101ffb895fc6ccb92709f4ad53e72087b31fa
sakurawald <sakurawald@gmail.com>
2025-01-08 21:34:32 +0800

    feature: add option `enable_back_on_death` and `enable_back_on_teleport`

--- src/main/java/io/github/sakurawald/module/initializer/back/config/model/BackConfigModel.java
+++ src/main/java/io/github/sakurawald/module/initializer/back/config/model/BackConfigModel.java
@@ -2,4 +2,6 @@ package io.github.sakurawald.module.initializer.back.config.model;
 
 public class BackConfigModel {
     public double ignore_distance = 32d;
+    public boolean enable_back_on_death = true;
+    public boolean enable_back_on_teleport = true;
 }
